6117051f15c6cfe48e02d847b9e13b88fda64d08
mozok <mozokevgen@gmail.com>
2018-06-20 09:53:05 +0300

    Make simple navbar

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map',
+  devtool: 'none',
   devServer: {
     contentBase: './dist'
   },
